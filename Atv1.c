34be107c894220b2198a3756ffc8e42066469586
Vinicius <viniciuspereiralira@hotmail.com>
2025-09-07 14:40:55 -0300

    Recebendo o valor N

---
+++ Atv1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Digite o valor do inteiro:\n");
+    int valor;
+    scanf("%d", &valor);
+    printf("Você digitou: %d\n", valor);
+    return 0;
+}
